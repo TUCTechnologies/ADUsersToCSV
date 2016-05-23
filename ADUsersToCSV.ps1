656b43eb4ff8340c51bf40587ce292a6b829ca79
Brandon <willjasen@gmail.com>
2016-05-23 16:49:29 -0400

    remove first, unneeded line in output file

--- ADUsersToCSV.ps1
+++ ADUsersToCSV.ps1
@@ -3,6 +3,8 @@
 Import-Module ActiveDirectory
 
 $SearchBase = ""
+$File = "ADUsers.csv"
+$FileTemp = "temp.csv"
 
 # Get user input
 $DomainName = Read-Host "Enter the domain name"
@@ -23,4 +25,11 @@ $SearchBase = $SearchBase.Substring(0,$SearchBase.Length-1)
 Write-Host "Searching: " $SearchBase
 
 # Return a list of users
-Get-ADUser -Filter * -SearchBase $SearchBase -Properties EmailAddress | Where-Object {$_.Enabled -eq $True} | Select-Object SAMAccountName, GivenName, Surname, EmailAddress
+$Users = Get-ADUser -Filter * -SearchBase $SearchBase -Properties EmailAddress | Where-Object {$_.Enabled -eq $True} | Select-Object SAMAccountName, GivenName, Surname, EmailAddress
+
+$Users | Export-Csv $File
+
+Get-Content $File |
+    Select -Skip 1 |
+    Set-Content "$FileTemp"
+Move "$FileTemp" $File -Force
